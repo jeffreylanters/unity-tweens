79fa977446526eb45fb69356ac0daa353e297678
Jeffrey Lanters <jeffreylanters@me.com>
2020-04-29 15:57:01 +0200

    Update TweenInstance.cs

--- Runtime/Core/TweenInstance.cs
+++ Runtime/Core/TweenInstance.cs
@@ -5,16 +5,21 @@ namespace ElRaccoone.Tweens.Core {
   /// Instance class for all Tweens.
   public abstract class TweenInstance : MonoBehaviour {
     internal bool isDecommissioned = false;
-
-    /// Cancel the tween and decommision the component.
-    public void Cancel () {
-      this.Decommission ();
-    }
+    
+    private Action onCancel = null;
+    private bool hasOnCancel = false;
 
     /// Destroys the component.
     internal void Decommission () {
       this.isDecommissioned = true;
       Object.Destroy (this);
+    }
+
+    /// Cancel the tween and decommision the component.
+    public void Cancel () {
+      if (this.hasOnCancel == true)
+        this.onCancel();
+      this.Decommission ();
     }
   }
 }
