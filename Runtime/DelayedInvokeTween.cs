7bd2c8f0c2dfa203a39c19a0f8e6f29844895daa
Jeffrey Lanters <jeffreylanters@me.com>
2020-04-29 17:35:13 +0200

    Update DelayedInvokeTween.cs

--- Runtime/DelayedInvokeTween.cs
+++ Runtime/DelayedInvokeTween.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 namespace ElRaccoone.Tweens {
   public static class DelayedInvokeTween {
-    public static Tween<bool> TweenDelayedInvoke (this Component self, bool to, float duration) =>
-      self.gameObject.TweenDelayedInvoke (duration, to);
+    public static Tween<bool> TweenDelayedInvoke (this Component self, float duration, Action onComplete) =>
+      self.gameObject.TweenDelayedInvoke (duration, onComplete);
 
-    public static Tween<bool> TweenDelayedInvoke (this GameObject self, bool to, float duration) =>
-      self.AddComponent<Tween> ().Finalize (duration, to);
+    public static Tween<bool> TweenDelayedInvoke (this GameObject self, float duration, Action onComplete) =>
+      self.AddComponent<Tween> ().SetOnComplete (onComplete).Finalize (duration, false);
 
     private class Tween : Tween<bool> {
       public override bool OnInitialize () {
