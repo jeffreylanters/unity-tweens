5e708e5eba2c9aec512fe1f6d3584284b77bccc8
Jeffrey Lanters <jeffreylanters@me.com>
2021-03-20 20:41:00 +0100

    Added Camera Field of View Tween

---
+++ Runtime/CameraFieldOfViewTween.cs
@@ -0,0 +1,30 @@
+using ElRaccoone.Tweens.Core;
+using UnityEngine;
+
+namespace ElRaccoone.Tweens {
+  public static class CameraFieldOfViewTween {
+    public static Tween<float> TweenCameraFieldOfView (this Component self, float to, float duration) =>
+      Tween<float>.Add<Driver> (self).Finalize (duration, to);
+
+    public static Tween<float> TweenCameraFieldOfView (this GameObject self, float to, float duration) =>
+      Tween<float>.Add<Driver> (self).Finalize (duration, to);
+
+    private class Driver : Tween<float> {
+      private new Camera camera;
+
+      public override bool OnInitialize () {
+        this.camera = this.gameObject.GetComponent<Camera> ();
+        return this.camera != null;
+      }
+
+      public override float OnGetFrom () {
+        return this.camera.fieldOfView;
+      }
+
+      public override void OnUpdate (float easedTime) {
+        this.valueCurrent = this.InterpolateValue (this.valueFrom, this.valueTo, easedTime);
+        this.camera.fieldOfView = this.valueCurrent;
+      }
+    }
+  }
+}
