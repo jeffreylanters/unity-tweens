6e5c5e90be5491515d6697448c08d2adf03507fd
Jeffrey Lanters <jeffreylanters@me.com>
2021-04-21 21:51:16 +0200

    Implemented Vector2 and Vector3 value tweens

---
+++ Runtime/ValueVector2Tween.cs
@@ -0,0 +1,39 @@
+using System;
+using ElRaccoone.Tweens.Core;
+using UnityEngine;
+
+namespace ElRaccoone.Tweens {
+  public static class ValueVector2Tween {
+    public static Tween<Vector2> TweenValueVector2 (this Component self, Vector2 to, float duration, Action<Vector2> onUpdate) =>
+      Tween<Vector2>.Add<Driver> (self).SetOnUpdate (onUpdate).Finalize (duration, to);
+
+    public static Tween<Vector2> TweenValueVector2 (this GameObject self, Vector2 to, float duration, Action<Vector2> onUpdate) =>
+      Tween<Vector2>.Add<Driver> (self).SetOnUpdate (onUpdate).Finalize (duration, to);
+
+    private class Driver : Tween<Vector2> {
+      private Action<Vector2> onUpdate = null;
+      private bool hasOnUpdate = false;
+
+      public override bool OnInitialize () {
+        return true;
+      }
+
+      public override Vector2 OnGetFrom () {
+        return Vector2.zero;
+      }
+
+      public override void OnUpdate (float easedTime) {
+        this.valueCurrent.x = this.InterpolateValue (this.valueFrom.x, this.valueTo.x, easedTime);
+        this.valueCurrent.y = this.InterpolateValue (this.valueFrom.y, this.valueTo.y, easedTime);
+        if (this.hasOnUpdate == true)
+          this.onUpdate (this.valueCurrent);
+      }
+
+      public Tween<Vector2> SetOnUpdate (Action<Vector2> onUpdate) {
+        this.onUpdate = onUpdate;
+        this.hasOnUpdate = true;
+        return this;
+      }
+    }
+  }
+}
---
+++ Runtime/ValueVector3Tween.cs
@@ -0,0 +1,40 @@
+using System;
+using ElRaccoone.Tweens.Core;
+using UnityEngine;
+
+namespace ElRaccoone.Tweens {
+  public static class ValueVector3Tween {
+    public static Tween<Vector3> TweenValueVector3 (this Component self, Vector3 to, float duration, Action<Vector3> onUpdate) =>
+      Tween<Vector3>.Add<Driver> (self).SetOnUpdate (onUpdate).Finalize (duration, to);
+
+    public static Tween<Vector3> TweenValueVector3 (this GameObject self, Vector3 to, float duration, Action<Vector3> onUpdate) =>
+      Tween<Vector3>.Add<Driver> (self).SetOnUpdate (onUpdate).Finalize (duration, to);
+
+    private class Driver : Tween<Vector3> {
+      private Action<Vector3> onUpdate = null;
+      private bool hasOnUpdate = false;
+
+      public override bool OnInitialize () {
+        return true;
+      }
+
+      public override Vector3 OnGetFrom () {
+        return Vector3.zero;
+      }
+
+      public override void OnUpdate (float easedTime) {
+        this.valueCurrent.x = this.InterpolateValue (this.valueFrom.x, this.valueTo.x, easedTime);
+        this.valueCurrent.y = this.InterpolateValue (this.valueFrom.y, this.valueTo.y, easedTime);
+        this.valueCurrent.z = this.InterpolateValue (this.valueFrom.z, this.valueTo.z, easedTime);
+        if (this.hasOnUpdate == true)
+          this.onUpdate (this.valueCurrent);
+      }
+
+      public Tween<Vector3> SetOnUpdate (Action<Vector3> onUpdate) {
+        this.onUpdate = onUpdate;
+        this.hasOnUpdate = true;
+        return this;
+      }
+    }
+  }
+}
