59b583a8b786d7e3b53a9802b5e6a5cf0b0c79be
Jeffrey Lanters <jeffreylanters@me.com>
2021-05-12 16:16:42 +0200

    Reformatted code using omnisharp formatter

--- Runtime/VolumeWeightTween.cs
+++ Runtime/VolumeWeightTween.cs
@@ -1,41 +1,35 @@
-#if SRP_AVAILABLE
+#if UNITY_RENDER_PIPELINES_CORE
 
 using ElRaccoone.Tweens.Core;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-namespace ElRaccoone.Tweens
-{
-    public static class VolumeWeightTween
-    {
-        public static Tween<float> TweenVolumeWeight(this Component self, float to, float duration) =>
-          Tween<float>.Add<Driver>(self).Finalize(duration, to);
+namespace ElRaccoone.Tweens {
+  public static class VolumeWeightTween {
+    public static Tween<float> TweenVolumeWeight (this Component self, float to, float duration) =>
+      Tween<float>.Add<Driver> (self).Finalize (duration, to);
 
-        public static Tween<float> TweenVolumeWeight(this GameObject self, float to, float duration) =>
-          Tween<float>.Add<Driver>(self).Finalize(duration, to);
+    public static Tween<float> TweenVolumeWeight (this GameObject self, float to, float duration) =>
+      Tween<float>.Add<Driver> (self).Finalize (duration, to);
 
-        private class Driver : Tween<float>
-        {
-            private Volume volume;
+    private class Driver : Tween<float> {
+      private Volume volume;
 
-            public override bool OnInitialize()
-            {
-                this.volume = this.gameObject.GetComponent<Volume>();
-                return this.volume != null;
-            }
+      public override bool OnInitialize () {
+        this.volume = this.gameObject.GetComponent<Volume> ();
+        return this.volume != null;
+      }
 
-            public override float OnGetFrom()
-            {
-                return this.volume.weight;
-            }
+      public override float OnGetFrom () {
+        return this.volume.weight;
+      }
 
-            public override void OnUpdate(float easedTime)
-            {
-                this.valueCurrent = this.InterpolateValue(this.valueFrom, this.valueTo, easedTime);
-                this.volume.weight = this.valueCurrent;
-            }
-        }
+      public override void OnUpdate (float easedTime) {
+        this.valueCurrent = this.InterpolateValue (this.valueFrom, this.valueTo, easedTime);
+        this.volume.weight = this.valueCurrent;
+      }
     }
+  }
 }
 
 #endif
