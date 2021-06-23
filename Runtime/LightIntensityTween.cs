6e25bac849089847d22727a7da2964270b8f861d
Jeffrey Lanters <jeffreylanters@me.com>
2021-06-23 17:09:01 +0200

    Added new LightIntensity tween

---
+++ Runtime/LightIntensityTween.cs
@@ -0,0 +1,24 @@
+using ElRaccoone.Tweens.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ElRaccoone.Tweens {
+  public static class LightIntensityTween {
+    public static Tween<float> TweenLightIntensity (this Component self, float to, float duration) =>
+      Tween<float>.Add<Driver> (self).Finalize (duration, to);
+
+    public static Tween<float> TweenLightIntensity (this GameObject self, float to, float duration) =>
+      Tween<float>.Add<Driver> (self).Finalize (duration, to);
+
+    private class Driver : Tween<float, Light> {
+      public override float OnGetFrom () {
+        return this.component.intensity;
+      }
+
+      public override void OnUpdate (float easedTime) {
+        this.valueCurrent = this.InterpolateValue (this.valueFrom, this.valueTo, easedTime);
+        this.component.intensity = this.valueCurrent;
+      }
+    }
+  }
+}
