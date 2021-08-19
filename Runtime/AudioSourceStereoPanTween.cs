a4167055d61bc0716e45fa1e4ceaf1ac1095f67e
Jeffrey Lanters <jeffreylanters@me.com>
2021-08-19 14:44:01 +0200

    Added new Audio Source Stereo Pan Tween

---
+++ Runtime/AudioSourceStereoPanTween.cs
@@ -0,0 +1,23 @@
+using ElRaccoone.Tweens.Core;
+using UnityEngine;
+
+namespace ElRaccoone.Tweens {
+  public static class AudioSourceStereoPanTween {
+    public static Tween<float> TweenAudioSourceStereoPan (this Component self, float to, float duration) =>
+      Tween<float>.Add<Driver> (self).Finalize (duration, to);
+
+    public static Tween<float> TweenAudioSourceStereoPan (this GameObject self, float to, float duration) =>
+      Tween<float>.Add<Driver> (self).Finalize (duration, to);
+
+    private class Driver : Tween<float, AudioSource> {
+      public override float OnGetFrom () {
+        return this.component.panStereo;
+      }
+
+      public override void OnUpdate (float easedTime) {
+        this.valueCurrent = this.InterpolateValue (this.valueFrom, this.valueTo, easedTime);
+        this.component.panStereo = this.valueCurrent;
+      }
+    }
+  }
+}
