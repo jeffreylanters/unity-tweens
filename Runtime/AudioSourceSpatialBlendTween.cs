d25a38b535f109046f457b4c8902f78e36e015dd
Jeffrey Lanters <jeffreylanters@me.com>
2021-08-19 14:43:57 +0200

    Added new Audio Source Spatial Blend Tween

---
+++ Runtime/AudioSourceSpatialBlendTween.cs
@@ -0,0 +1,23 @@
+using ElRaccoone.Tweens.Core;
+using UnityEngine;
+
+namespace ElRaccoone.Tweens {
+  public static class AudioSourceSpatialBlendTween {
+    public static Tween<float> TweenAudioSourceSpatialBlend (this Component self, float to, float duration) =>
+      Tween<float>.Add<Driver> (self).Finalize (duration, to);
+
+    public static Tween<float> TweenAudioSourceSpatialBlend (this GameObject self, float to, float duration) =>
+      Tween<float>.Add<Driver> (self).Finalize (duration, to);
+
+    private class Driver : Tween<float, AudioSource> {
+      public override float OnGetFrom () {
+        return this.component.spatialBlend;
+      }
+
+      public override void OnUpdate (float easedTime) {
+        this.valueCurrent = this.InterpolateValue (this.valueFrom, this.valueTo, easedTime);
+        this.component.spatialBlend = this.valueCurrent;
+      }
+    }
+  }
+}
