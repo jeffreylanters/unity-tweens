764f50c9523c92df478168322257bd59163f840b
Jeffrey Lanters <jeffreylanters@me.com>
2021-08-19 14:49:36 +0200

    Marked Delayed Invoke Tweens deprecated

--- Runtime/DelayedInvokeTween.cs
+++ Runtime/DelayedInvokeTween.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 namespace ElRaccoone.Tweens {
   public static class DelayedInvokeTween {
+    [ObsoleteAttribute ("This method is deprecated, Unity Tweens will only focus on actual Tweens in the future. Please implement another way of invoke in method delayed.")]
     public static Tween<bool> TweenDelayedInvoke (this Component self, float duration, Action onComplete) =>
       Tween<bool>.Add<Driver> (self).SetOnComplete (onComplete).Finalize (duration, false);
 
+    [ObsoleteAttribute ("This method is deprecated, Unity Tweens will only focus on actual Tweens in the future. Please implement another way of invoke in method delayed.")]
     public static Tween<bool> TweenDelayedInvoke (this GameObject self, float duration, Action onComplete) =>
       Tween<bool>.Add<Driver> (self).SetOnComplete (onComplete).Finalize (duration, false);
 
