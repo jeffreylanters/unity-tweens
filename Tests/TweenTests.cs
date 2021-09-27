4d9deb3c6d45db9da03ddeb1ffda2f1952db7e10
Jeffrey Lanters <jeffreylanters@me.com>
2021-09-27 10:53:15 +0200

    Added Tests for Spot Angle and Light Range Tweens

--- Tests/TweenTests.cs
+++ Tests/TweenTests.cs
@@ -61,6 +61,10 @@ namespace ElRaccoone.Tweens.Tests {
         this.target.TweenLightColor (Color.red, 1).SetFrom (Color.white);
       if (GUILayout.Button ("LightIntensity"))
         this.target.TweenLightIntensity (10, 1).SetFrom (0);
+      if (GUILayout.Button ("LightRange"))
+        this.target.TweenLightRange (10, 1).SetFrom (0);
+      if (GUILayout.Button ("LightSpotAngle"))
+        this.target.TweenLightSpotAngle (10, 1).SetFrom (0);
       if (GUILayout.Button ("LocalEulerAngles"))
         this.target.TweenLocalRotation (Vector3.one * 45, 1).SetFrom (Vector3.zero);
       if (GUILayout.Button ("LocalEulerAnglesX"))
