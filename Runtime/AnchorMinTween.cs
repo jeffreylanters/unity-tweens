b70477b44d4faf9772e31d14bf7f4652200a9e59
Jeffrey Lanters <jeffreylanters@me.com>
2022-05-03 15:25:32 +0200

    Added new Tween animating a Rect Transform's Anchor Min and Max

---
+++ Runtime/AnchorMinTween.cs
@@ -0,0 +1,37 @@
+using ElRaccoone.Tweens.Core;
+using UnityEngine;
+
+namespace ElRaccoone.Tweens {
+  public static class AnchorMinTween {
+    public static Tween<Vector2> TweenAnchorMin (this Component self, Vector2 to, float duration) =>
+      Tween<Vector2>.Add<Driver> (self).Finalize (to, duration);
+
+    public static Tween<Vector2> TweenAnchorMin (this GameObject self, Vector2 to, float duration) =>
+      Tween<Vector2>.Add<Driver> (self).Finalize (to, duration);
+
+    /// <summary>
+    /// The driver is responsible for updating the tween's state.
+    /// </summary>
+    private class Driver : Tween<Vector2, RectTransform> {
+      
+      /// <summary>
+      /// Overriden method which is called when the tween starts and should
+      /// return the tween's initial value.
+      /// </summary>
+      public override Vector2 OnGetFrom () {
+        return this.component.anchorMin;
+      }
+
+      /// <summary>
+      /// Overriden method which is called every tween update and should be used
+      /// to update the tween's value.
+      /// </summary>
+      /// <param name="easedTime">The current eased time of the tween's step.</param>
+      public override void OnUpdate (float easedTime) {
+        this.valueCurrent.x = this.InterpolateValue (this.valueFrom.x, this.valueTo.x, easedTime);
+        this.valueCurrent.y = this.InterpolateValue (this.valueFrom.y, this.valueTo.y, easedTime);
+        this.component.anchorMin = this.valueCurrent;
+      }
+    }
+  }
+}
