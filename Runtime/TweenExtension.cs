519ee00882f823a7662debd0dd15170e6b6a0eca
Jeffrey Lanters <jeffreylanters@me.com>
2023-08-16 11:13:44 +0200

    Add multiple tweens at once

--- Runtime/TweenExtension.cs
+++ Runtime/TweenExtension.cs
@@ -9,6 +9,25 @@ namespace Tweens {
       return instance;
     }
 
+    public static TweenInstance[] AddTweens<ComponentType1, DataType1, ComponentType2, DataType2>(this GameObject target, Tween<ComponentType1, DataType1> tween1, Tween<ComponentType2, DataType2> tween2) where ComponentType1 : Component where ComponentType2 : Component {
+      var instances = new TweenInstance[] {
+        new TweenInstance<ComponentType1, DataType1>(target, tween1),
+        new TweenInstance<ComponentType2, DataType2>(target, tween2),
+      };
+      TweenEngine.instances.AddRange(instances);
+      return instances;
+    }
+
+    public static TweenInstance[] AddTweens<ComponentType1, DataType1, ComponentType2, DataType2, ComponentType3, DataType3>(this GameObject target, Tween<ComponentType1, DataType1> tween1, Tween<ComponentType2, DataType2> tween2, Tween<ComponentType3, DataType3> tween3) where ComponentType1 : Component where ComponentType2 : Component where ComponentType3 : Component {
+      var instances = new TweenInstance[] {
+        new TweenInstance<ComponentType1, DataType1>(target, tween1),
+        new TweenInstance<ComponentType2, DataType2>(target, tween2),
+        new TweenInstance<ComponentType3, DataType3>(target, tween3),
+      };
+      TweenEngine.instances.AddRange(instances);
+      return instances;
+    }
+
     public static void CancelTweens(this GameObject target, bool includeChildren = false) {
       var instances = TweenEngine.instances.FindAll(instance => {
         if (instance.target == target) {
