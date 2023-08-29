589de03bc3629b7b9c21ae556f4ea64ad0d46b3f
Jeffrey Lanters <jeffreylanters@me.com>
2023-08-29 12:14:21 +0200

    Added instance check to prevent domain-reload issues

--- Runtime/Core/TweenBehaviour.cs
+++ Runtime/Core/TweenBehaviour.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 namespace Tweens.Core {
   [AddComponentMenu("")]
   class TweenBehaviour : MonoBehaviour {
+    static TweenBehaviour instance;
+
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     internal static void Initialize() {
-      DontDestroyOnLoad(new GameObject("TweenBehaviour", typeof(TweenBehaviour)));
+      if (instance != null) {
+        return;
+      }
+      instance = new GameObject("TweenBehaviour").AddComponent<TweenBehaviour>();
+      DontDestroyOnLoad(instance.gameObject);
     }
 
     internal void LateUpdate() {
