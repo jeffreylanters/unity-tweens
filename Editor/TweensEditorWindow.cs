ed170039eb90f9eef90bcda1ae46c64abd74f2cd
Jeffrey Lanters <jeffreylanters@me.com>
2023-08-30 13:43:12 +0200

    Minor Tweens inspector changes

--- Editor/TweensEditorWindow.cs
+++ Editor/TweensEditorWindow.cs
@@ -14,6 +14,7 @@ namespace Tweens.Editor {
     const string autoRepaintKey = baseKey + ".autoRepaint";
     Vector2 scrollPosition;
     string searchQuery;
+    bool showProgress;
     bool showDirection;
     bool showIsPaused;
     bool showPingPongInterval;
@@ -42,20 +43,21 @@ namespace Tweens.Editor {
       var wasEnabled = GUI.enabled;
       GUI.enabled = Application.isPlaying;
       EditorGUILayout.BeginHorizontal(EditorStyles.toolbar);
-      if (GUILayout.Button("Cancel all", EditorStyles.toolbarButton)) {
+      if (GUILayout.Button(new GUIContent("Cancel all", "Cancels all of the instantiated tweens."), EditorStyles.toolbarButton)) {
         TweenEngine.instances.ForEach(tweenInstance => tweenInstance.Cancel());
       }
-      if (GUILayout.Button("Pause all", EditorStyles.toolbarButton)) {
+      if (GUILayout.Button(new GUIContent("Pause all", "Pauses all of the instantiated tweens."), EditorStyles.toolbarButton)) {
         TweenEngine.instances.ForEach(tweenInstance => tweenInstance.isPaused = true);
       }
-      if (GUILayout.Button("Unpause all", EditorStyles.toolbarButton)) {
+      if (GUILayout.Button(new GUIContent("Unpause all", "Unpauses all of the instantiated tweens."), EditorStyles.toolbarButton)) {
         TweenEngine.instances.ForEach(tweenInstance => tweenInstance.isPaused = false);
       }
       GUILayout.FlexibleSpace();
       GUI.enabled = true;
       searchQuery = EditorGUILayout.TextField(searchQuery, EditorStyles.toolbarSearchField);
-      if (GUILayout.Button(EditorGUIUtility.IconContent("d_SceneViewVisibility@2x"), EditorStyles.toolbarDropDown)) {
+      if (GUILayout.Button(new GUIContent(EditorGUIUtility.IconContent("d_SceneViewVisibility@2x").image, "Toggle the visibility of various columns."), EditorStyles.toolbarDropDown)) {
         var menu = new GenericMenu();
+        menu.AddItem(new GUIContent("Show Progress"), showProgress, () => EditorPrefs.SetBool(baseKey + ".showProgress", showProgress = !showProgress));
         menu.AddItem(new GUIContent("Show Direction"), showDirection, () => EditorPrefs.SetBool(showDirectionKey, showDirection = !showDirection));
         menu.AddItem(new GUIContent("Show Paused"), showIsPaused, () => EditorPrefs.SetBool(showIsPausedKey, showIsPaused = !showIsPaused));
         menu.AddItem(new GUIContent("Show Ping Pong Interval"), showPingPongInterval, () => EditorPrefs.SetBool(showPingPongIntervalKey, showPingPongInterval = !showPingPongInterval));
@@ -68,26 +70,28 @@ namespace Tweens.Editor {
       scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
       EditorGUILayout.BeginHorizontal();
       GUILayout.Space(10);
-      EditorGUILayout.LabelField("Target", EditorStyles.miniBoldLabel);
+      EditorGUILayout.LabelField(new GUIContent("Target", "The target gameObject on which the tween is attached."), EditorStyles.miniBoldLabel);
       GUILayout.FlexibleSpace();
-      EditorGUILayout.LabelField("Progress", EditorStyles.miniLabel, GUILayout.Width(100));
-      EditorGUILayout.LabelField("Time", EditorStyles.miniLabel, GUILayout.Width(50));
-      EditorGUILayout.LabelField("Duration", EditorStyles.miniLabel, GUILayout.Width(50));
-      EditorGUILayout.LabelField("Halt", EditorStyles.miniLabel, GUILayout.Width(50));
+      if (showProgress) {
+        EditorGUILayout.LabelField(new GUIContent("Progress", "The progress position of the tween."), EditorStyles.miniLabel, GUILayout.Width(50));
+      }
+      EditorGUILayout.LabelField(new GUIContent("Time", "The current playback position."), EditorStyles.miniLabel, GUILayout.Width(50));
+      EditorGUILayout.LabelField(new GUIContent("Duration", "The duration of the Tween."), EditorStyles.miniLabel, GUILayout.Width(50));
+      EditorGUILayout.LabelField(new GUIContent("Halt", "A timer indicating how long the tween is halted."), EditorStyles.miniLabel, GUILayout.Width(50));
       if (showLoops) {
-        EditorGUILayout.LabelField("Loops", EditorStyles.miniLabel, GUILayout.Width(50));
+        EditorGUILayout.LabelField(new GUIContent("Loops", "The number of loops the tween has to go though left."), EditorStyles.miniLabel, GUILayout.Width(50));
       }
       if (showDirection) {
-        EditorGUILayout.LabelField("Direction", EditorStyles.miniLabel, GUILayout.Width(50));
+        EditorGUILayout.LabelField(new GUIContent("Direction", "The playback direction of the tween."), EditorStyles.miniLabel, GUILayout.Width(50));
       }
       if (showIsPaused) {
-        EditorGUILayout.LabelField("Paused", EditorStyles.miniLabel, GUILayout.Width(50));
+        EditorGUILayout.LabelField(new GUIContent("Paused", "Whether the tween is paused."), EditorStyles.miniLabel, GUILayout.Width(50));
       }
       if (showPingPongInterval) {
-        EditorGUILayout.LabelField("PingPong", EditorStyles.miniLabel, GUILayout.Width(50));
+        EditorGUILayout.LabelField(new GUIContent("PingPong", "The interval between PingPong playbacks."), EditorStyles.miniLabel, GUILayout.Width(50));
       }
       if (showRepeatInterval) {
-        EditorGUILayout.LabelField("Repeat", EditorStyles.miniLabel, GUILayout.Width(50));
+        EditorGUILayout.LabelField(new GUIContent("Repeat", "The interval between playbacks."), EditorStyles.miniLabel, GUILayout.Width(50));
       }
       GUILayout.Space(10);
       EditorGUILayout.EndHorizontal();
@@ -119,9 +123,11 @@ namespace Tweens.Editor {
           }
         }
         GUILayout.FlexibleSpace();
-        EditorGUILayout.LabelField("Progress", GUILayout.Width(100));
-        EditorGUI.ProgressBar(GUILayoutUtility.GetLastRect(), tweenInstance.time / tweenInstance.duration, $"{Mathf.Round(tweenInstance.time / tweenInstance.duration * 100)}%");
-        EditorGUILayout.LabelField($"{tweenInstance.time:0.00}s", GUILayout.Width(100));
+        if (showProgress) {
+          EditorGUILayout.LabelField("Progress", GUILayout.Width(50));
+          EditorGUI.ProgressBar(GUILayoutUtility.GetLastRect(), tweenInstance.time / tweenInstance.duration, $"{Mathf.Round(tweenInstance.time / tweenInstance.duration * 100)}%");
+        }
+        EditorGUILayout.LabelField($"{tweenInstance.time:0.00}s", GUILayout.Width(50));
         EditorGUILayout.LabelField($"{tweenInstance.duration:0.00}s", GUILayout.Width(50));
         EditorGUILayout.LabelField(tweenInstance.haltTime != null ? $"{tweenInstance.haltTime:0.00}s" : "-", GUILayout.Width(50));
         if (showLoops) {
