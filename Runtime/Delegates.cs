fbc541d13e29e666d63576ade35cc4e97532b61c
Jeffrey Lanters <jeffreylanters@me.com>
2023-08-30 14:34:10 +0200

    Updated delegate protection levels

--- Runtime/Delegates.cs
+++ Runtime/Delegates.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
 namespace Tweens {
-  public delegate DataType LerpDelegate<DataType>(DataType from, DataType to, float time);
-  public delegate void ApplyDelegate<ComponentType, DataType>(ComponentType component, DataType value);
-  public delegate float EaseFunctionDelegate(float time);
+  internal delegate DataType LerpDelegate<DataType>(DataType from, DataType to, float time);
+  internal delegate void ApplyDelegate<ComponentType, DataType>(ComponentType component, DataType value);
+  internal delegate float EaseFunctionDelegate(float time);
   public delegate void OnUpdateDelegate<ComponentType, DataType>(TweenInstance<ComponentType, DataType> instance, DataType value) where ComponentType : Component;
   public delegate void OnAddDelegate<ComponentType, DataType>(TweenInstance<ComponentType, DataType> instance) where ComponentType : Component;
   public delegate void OnStartDelegate<ComponentType, DataType>(TweenInstance<ComponentType, DataType> instance) where ComponentType : Component;
