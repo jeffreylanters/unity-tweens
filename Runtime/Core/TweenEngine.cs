dd12eb547bfa0f0b2b1c2bff524835aee28a7b29
Alex Meesters <alex_meesters@msn.com>
2023-08-31 20:09:30 +0200

    Changed TweenEngine.cs to support custom domain reload settings.

--- Runtime/Core/TweenEngine.cs
+++ Runtime/Core/TweenEngine.cs
@@ -1,8 +1,16 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Tweens.Core {
   static class TweenEngine {
     readonly internal static List<TweenInstance> instances = new();
+
+    #if UNITY_EDITOR
+    [RuntimeInitializeOnLoadMethod (RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void ClearInstances () {
+      instances.Clear ();
+    }
+    #endif
 
     internal static void Update() {
       for (var i = 0; i < instances.Count; i++) {
